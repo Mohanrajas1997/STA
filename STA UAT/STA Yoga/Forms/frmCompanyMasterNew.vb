e308636590585a7ed9c24083a88e42dc111d9b76
mohanraja.s@flexicodeindia.com <mohanraja.s@flexicodeindia.com>
2025-11-24 13:46:04 +0530

    640 to 630 height

--- STA UAT/STA Yoga/Forms/frmCompanyMasterNew.vb
+++ STA UAT/STA Yoga/Forms/frmCompanyMasterNew.vb
@@ -3,7 +3,7 @@
 Public Class frmCompanyMasterNew
 
     Private Sub frmCompany_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Height = 640
+        Me.Height = 630
         Dim lsSql As String
         lnkAddAttachment.Enabled = False
         ' Security type
